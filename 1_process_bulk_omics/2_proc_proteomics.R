eeda7ff2d014626c9d356ba2c711cb23785ce0d5
jessica-ewald <33487514+jessica-ewald@users.noreply.github.com>
2024-04-06 15:16:19 -0400

    make paths relative in 2_proc_proteomics.R

--- 1_process_bulk_omics/2_proc_proteomics.R
+++ 1_process_bulk_omics/2_proc_proteomics.R
@@ -1,6 +1,7 @@
 # Proteomics processing
-# Jessica Ewald
-# From the October 12, 2022 script; adapted Feb 13, 2023 for updated dataset; adapted Apr 25 for web-tool
+# Author: Jessica Ewald
+
+## Set your working directory to the "1_process_bulk_omics" directory
 
 library(WGCNA)
 library(dplyr)
@@ -8,12 +9,20 @@ library(ggplot2)
 library(imp4p)
 library(RSQLite)
 
+source("../set_paths.R")
+setPaths()
+
+raw.omics.path <- paste0(other.tables.path, "omics_processing_input/raw/")
+proc.omics.path <- paste0(other.tables.path, "omics_processing_input/proc/")
+
+if(!dir.exists(proc.omics.path)){ dir.create(proc.omics.path) }
+
 # RF works well for proteomics data: https://www.nature.com/articles/s41598-021-81279-4#:~:text=Missing%20value%20imputation%20is%20a,and%20disadvantages%20of%20each%20method
 # imp4p requires log-normalized data (takes a long time to run)
 # imp4p RF just a wrapper for 'MissForest' package: https://academic.oup.com/bioinformatics/article/28/1/112/219101
 
 # get proteomics data
-prot <- read.csv("/Users/jessicaewald/Library/CloudStorage/OneDrive-McGillUniversity/XiaLab/Tools/HumanIslets/Omics_data/raw/raw_prot.csv")
+prot <- read.csv(paste0(raw.omics.path, "raw_prot.csv"))
 prot.ids <- prot$X
 rownames(prot) <- prot$X
 prot <- prot[,-1]
@@ -33,11 +42,10 @@ prot[!is.na(prot)] <- log2(prot[!is.na(prot)])
 boxplot(prot[,rand.inds], main = "Log transformed")
 
 prot <- impute.RF(prot, conditions = factor(rep("1", dim(prot)[2])), verbose = TRUE)
-saveRDS(prot, "/Users/jessicaewald/Desktop/prot_imputed.rds") # save copy in case of an issue in the following steps
 boxplot(prot[,rand.inds], main = "Imputed")
 
 # annotate to Entrez
-myDb <- dbConnect(SQLite(), "/Users/jessicaewald/Library/CloudStorage/OneDrive-McGillUniversity/XiaLab/Tools/HumanIslets/annotation_libraries/hsa_genes.sqlite")
+myDb <- dbConnect(SQLite(), paste0(other.tables.path, "libraries/hsa_genes.sqlite"))
 uniprot <- dbReadTable(myDb, "entrez_uniprot")
 entrez <- dbReadTable(myDb, "entrez")
 dbDisconnect(myDb)
@@ -61,4 +69,4 @@ prot.keep <- which(prot.var > var.thresh)
 prot <- prot[prot.keep, ]
 
 # write out results
-write.csv(prot, "/Users/jessicaewald/Library/CloudStorage/OneDrive-McGillUniversity/XiaLab/Tools/HumanIslets/Omics_data/proc/proc_prot.csv")
+write.csv(prot, paste0(proc.omics.path, "proc_prot.csv"))
